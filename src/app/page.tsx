87a10e3665821e3a1c2132dfa4ad493bc849242d
ovidiuteodor <ovidiu.teodor.c@gmail.com>
2026-08-08 21:28:47 +0300

    Adaugat butoane

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 function CardProiect(props: { titlu: string; descriere: string }) {
   return (
     <div className="rounded-xl border border-gray-200 p-6 shadow-sm">
@@ -27,11 +31,41 @@ export default function Home() {
     {id: 2, functie: "Frontend Developer", companie: "Facebook", anStart: 2018 },
   ];
 
+  const [contor, setContor] = useState(0);
+
+  const [ascuns, setAscuns] = useState(false);
+
   return (
     <main className="mx-auto max-w-3xl p-8">
+      
       <h1 className="text-4xl font-bold text-blue-600">My Identity</h1>
 
-      <h2 className="text-2xl font-semibold mt-8">Proiecte</h2>
+    <div className="flex flex-col gap-2">
+    <button
+      onClick={() => setContor(contor + 1)}
+      className="rounded-lg bg-blue-600 px-4 py-2 text-white"
+      >
+      Ai apasat de {contor} ori
+    </button>
+
+    <button
+      onClick={() => setContor(0)}
+      className="rounded-lg bg-blue-600 px-4 py-2 text-white"
+      >
+      Reseteaza butonul
+    </button>
+    
+    <button
+      onClick={() => setAscuns(!ascuns)}
+      className="rounded-lg bg-blue-600 px-4 py-2 text-white"
+      >
+      {ascuns ? "Arata" : "Ascunde"} secțiunea de proiecte
+    </button>
+
+    </div>
+
+      <h2 className="text-2xl font-semibold mt-8 ">Proiecte</h2>
+      {!ascuns &&
       <div className="flex flex-col gap-4 mt-8">
       {proiecte.map((proiect) => (
         <CardProiect
@@ -41,6 +75,7 @@ export default function Home() {
         />
       ))}
       </div>
+}
 
       <h2 className="text-2xl font-semibold mt-8">Experienta</h2>
       <div className="flex flex-col gap-4 mt-8">
